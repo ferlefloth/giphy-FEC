944225dd0bdc14face6affc9dafd1e2ea9a1ef59
Mello <mellolefloth@gmail.com>
2022-04-07 00:40:08 -0300

    added fetch with baseUrl and apiKey

--- giphychallenge/src/components/SearchBox.js
+++ giphychallenge/src/components/SearchBox.js
@@ -4,21 +4,24 @@ import { useState, useEffect } from "react";
 function SearchBox(){
     const [username,setUsername] =useState('');
 
-    const sendSomething = (event)=>{
-        event.preventDefault();
-        fetch('https://api.giphy.com/v1/gifs/search?api_key=oy5WoJCYpGcZe3cYmmFvEydLBVSZamUR&q=cat&limit=25&offset=0&rating=g&lang=en')
+    const sendSomething = (event)=>{ //el event es el username
+       event.preventDefault();
+       const baseUrl='https://api.giphy.com/v1/gifs/search?' 
+       const api_key= 'oy5WoJCYpGcZe3cYmmFvEydLBVSZamUR'
+        fetch(`${baseUrl}api_key=${api_key}&q=${event}&limit=25&offset=0&rating=g&lang=en`)
         .then(res => res.json())
         .then(data =>{
            const allElements = data.data;
            for( let element of allElements){
                // Acá se debería pushear la data
                //NUEVO CAMBIOS
-               console.log("los ids son : " + element.id)
+               console.log("los ids son : " + JSON.stringify(element))
            }
         })
     }
 
     const handleChange = (event)=>{
+        console.log("el event : " + event.target.value)
         setUsername(event.target.value.toLowerCase());
 
     }
