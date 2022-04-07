cd151ab88574b25949c69728f166678bb9098c8a
Mello <mellolefloth@gmail.com>
2022-04-07 04:07:08 -0300

    added new library MaterialUI, Trying to communicate between two components and render the images

--- giphychallenge/src/App.js
+++ giphychallenge/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import SearchBox from './components/SearchBox';
-
+import ImageList from './components/ImageList';
 import './App.css';
 
 function App() {
@@ -8,20 +8,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <SearchBox>
-       
+        
         </SearchBox>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ImageList>
+        </ImageList>
       </header>
     </div>
   );
--- giphychallenge/src/components/SearchBox.js
+++ giphychallenge/src/components/SearchBox.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+import React, { Component } from "react";
 import { useState, useEffect } from "react";
-
+import ImageList from './ImageList';
 function SearchBox(){
     const [username,setUsername] =useState('');
 
+    
     const sendSomething = (event)=>{ //el event es el username
        event.preventDefault();
        const baseUrl='https://api.giphy.com/v1/gifs/search?' 
@@ -13,11 +13,14 @@ function SearchBox(){
         .then(res => res.json())
         .then(data =>{
            const allElements = data.data;
+           let allUrls = [];
            for( let element of allElements){
                // Acá se debería pushear la data
                //NUEVO CAMBIOS
-               console.log("los ids son : " + JSON.stringify(element))
+               allUrls.push(element.url);
            }
+           console.log("todas las urls: " + JSON.stringify( allUrls));
+           return allUrls;
         })
     }
 
@@ -26,7 +29,7 @@ function SearchBox(){
         setUsername(event.target.value.toLowerCase());
 
     }
-
+    
     return(
         <>
             <form onSubmit={sendSomething}>
@@ -41,6 +44,7 @@ function SearchBox(){
                 </div>
                 <button type="submit">Enviar</button>
             </form>
+          
         </>
     )
 }
